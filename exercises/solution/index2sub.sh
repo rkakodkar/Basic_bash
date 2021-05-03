ea719bdbb07d21ac83713497802094a8220ec74e
Rohit Kakodkar <rohit_kakodkar@brown.edu>
2021-05-03 11:30:56 -0400

    Added if workflow in solutions

--- exercises/solution/index2sub.sh
+++ exercises/solution/index2sub.sh
@@ -5,9 +5,23 @@ function ind2sub() {
 	y=$(($1/5))
 }
 
+# using the function
+ind2sub 13
+
+# using with for loops
 for ((i=0;i<25;i++)){
 	ind2sub $i
 	echo "x index = $x; y index = $y"
+}
+
+echo "----------------------"
+
+# complecated workflows
+for ((i=0;i<25;i++)){
+	ind2sub $i
+	if [ $x -ne 2 ]; then
+		echo "x index = $x; y index = $y"
+	fi
 }
 
 echo "Done"
